f253014c3a883b852ad8937d8663e2fb1a57fc48
roberto <roberto.sodini1@gmail.com>
2025-07-18 15:10:15 +0200

    Initial commit

---
+++ src/main/java/com/app_bancaria/my_bnl_application/MyBnlApplication.java
@@ -0,0 +1,13 @@
+package com.app_bancaria.my_bnl_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBnlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBnlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app_bancaria/my_bnl_application/MyBnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app_bancaria.my_bnl_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
